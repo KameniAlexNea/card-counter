719e9dc1518a7a16c45a65c30dfabffc43640ee5
eak <eak@ivalua.com>
2025-01-31 18:43:34 +0000

    Refactor OCR functionality to use EasyOCR; update requirements and add Firebase management

--- ocr_engine.py
+++ ocr_engine.py
@@ -1,38 +1,22 @@
-# Load model directly
-import os
-import torch
-from transformers import AutoModel, AutoTokenizer
 from PIL import Image
-import uuid
+import easyocr
+import io
+from loguru import logger
 
-UPLOAD_FOLDER = "./uploads"
-RESULTS_FOLDER = "./results"
 
-for folder in [UPLOAD_FOLDER, RESULTS_FOLDER]:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-class OCRModel:
-    tokenizer: AutoTokenizer
-    model: AutoModel
-
+class EasyOCRReader:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-        self.model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda' if torch.cuda.is_available() else "cpu", use_safetensors=True, pad_token_id=self.tokenizer.eos_token_id)
-        self.model = self.model.eval()
-        if torch.cuda.is_available():
-            self.model = self.model.cuda()
+        self.reader = easyocr.Reader(["fr", "en"], gpu=False)
 
-    def chat(self, image: Image.Image) -> str:
-        unique_id = str(uuid.uuid4())
-        image_path = os.path.join(UPLOAD_FOLDER, f"{unique_id}.png")
-        result_path = os.path.join(RESULTS_FOLDER, f"{unique_id}.txt")
+    def chat(self, image: Image.Image):
+        # Convert image to bytes
+        img_byte_arr = io.BytesIO()
+        image.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
 
-        image.save(image_path)
-        
-        res = self.model.chat(self.tokenizer, image_path, ocr_type='ocr')
-        with open(result_path, 'w') as f:
-            f.write(res)
-        return res
+        # Call readtext with image bytes
+        texts = self.reader.readtext(img_byte_arr, detail=0)
+        logger.info(" ".join(texts))
+        return " ".join(texts)
 
-ocr_model = OCRModel()
+ocr_model = EasyOCRReader()
