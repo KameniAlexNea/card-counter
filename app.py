54b5c6df080f5e0fa0b940690dda2e3fd91c2901
eak <eak@ivalua.com>
2025-02-01 11:49:08 +0000

    Refactor Firebase integration to use dynamic collection name; enhance voter card verification with ID counting and improved error handling

--- app.py
+++ app.py
@@ -61,12 +61,18 @@ class FirebaseConnector:
         cred = credentials.Certificate(firebase_config)
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
+        self.collecttion_name = os.getenv("collection", "id-card")
 
     def add(self, ocr: str, key: str):
-        doc_ref = self.db.collection("id-card").document(key)
+        doc_ref = self.db.collection(self.collecttion_name).document(key)
         if doc_ref.get().exists:
             raise ValueError(f"Document with key {key} already exists.")
-        return self.db.collection("id-card").add({"ocr": ocr}, document_id=key)
+        return self.db.collection(self.collecttion_name).add({"ocr": ocr}, document_id=key)
+
+    def get_id_counted(self):
+        docs = self.db.collection(self.collecttion_name).stream()
+        count = sum(1 for _ in docs)
+        return count
 
 
 def search_element(words: list[str], target: str, thr: float = 60):
@@ -79,8 +85,11 @@ class VoterCardVerifier:
     def __init__(self):
         self.db = FirebaseConnector()
         self.ocr_engine = EasyOCRReader()
+        self.thr = int(os.getenv("thr", 75))
+        self.n_cards = 0
 
-    def verify_card(self, image: Union[str, Image.Image], card_number, thr: float = 60):
+    def verify_card(self, image: Union[str, Image.Image], card_number,):
+        self.n_cards = self.get_id_counted()
         # Convertir l'image pour Groq
         if isinstance(image, str):
             image = Image.open(image)
@@ -88,51 +97,62 @@ class VoterCardVerifier:
         # Préparer la requête pour le modèle et l'appeler
         ocr_data = self.ocr_engine.chat(image)
 
-        score1 = search_element(ocr_data, "camer", thr)
-        score2 = search_element(ocr_data, card_number, thr)
+        score1 = search_element(ocr_data, "camer", self.thr)
+        score2 = search_element(ocr_data, card_number, self.thr)
 
         ocr_data = score1 + score2
         logger.info(str(ocr_data))
 
         # valider les entrées
         is_valid = len(score1) and len(score2)
-
-        if is_valid:
-            # Sauvegarder dans Firebase
-            self.save_card_number(ocr_data, card_number)
-            return "Your ID Card have been recorded, thank you !!!"
-        else:
-            return "Numéro de carte non trouvé sur l'image"
+        try:
+            if is_valid:
+                # Sauvegarder dans Firebase
+                self.save_card_number(ocr_data, card_number)
+                self.n_cards += 1
+                return f"Your ID Card have been recorded, thank you !!! Num Card: {self.n_cards}"
+            else:
+                return f"Card number not found, please improve the quality of image passed. Num Card: {self.n_cards}"
+        except ValueError as ex:
+            return f"ID Card already saved in the database. Num Card: {self.n_cards}"
 
     def save_card_number(self, ocr_data, card_number):
         return self.db.add(str(ocr_data), str(card_number))
+
+    def get_id_counted(self):
+        return self.db.get_id_counted()
 
 
 # Interface Gradio
 def create_interface():
     verifier = VoterCardVerifier()
+    description = (
+        "Card ID Counter is an application designed to count voter card numbers "
+        "by analyzing uploaded images using OCR technology. It ensures accuracy by "
+        "checking if the provided number appears on the card and confirming its from Cameroon.\n\n"
+        "🌟 **Code Repository**: [Card ID Counter GitHub](https://github.com/Nganga-AI/card-counter)"
+    )
 
     def process(image, card_number: str):
         if not card_number.strip():
-            return "Veuillez entrer un numéro de carte"
+            return "Enter your elect count number"
         return verifier.verify_card(image, card_number)
 
     interface = gr.Interface(
         fn=process,
         inputs=[
-            gr.Image(type="pil", label="Image de la carte d'électeur"),
-            gr.Textbox(label="Numéro de carte"),
+            gr.Image(type="pil", label="Upload ID Card Image"),
+            gr.Textbox(label="Card Number"),
         ],
-        outputs=gr.Textbox(label="Résultat"),
-        title="Vérification de Carte d'Électeur",
-        description="Uploadez une image de votre carte d'électeur et entrez son numéro pour vérification",
+        outputs=gr.Textbox(label="Result"),
+        title="Voters ID Counter",
+        description=description,
     )
 
     return interface
 
 
 if __name__ == "__main__":
-
     # Créer et lancer l'interface
     demo = create_interface()
     demo.launch()
